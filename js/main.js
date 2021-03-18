fde3c3825bd34c5ea30657ef211f1057c3474df6
pf4pad <65763670+pf4pad@users.noreply.github.com>
2021-03-18 15:26:19 +0200

    burger

--- js/main.js
+++ js/main.js
@@ -1,22 +1,44 @@
  
    
-    function toggleMenu(params) {
+     function toggleMenu(params) {
 
-      const menuToggle = document.querySelector('.toggle');
-      const navugation = document.querySelector('.header__list');
-      menuToggle.classList.toggle('active')
-      navugation.classList.toggle('active')
+       const menuToggle = document.querySelector('.toggle');
+       const navugation = document.querySelector('.header__list');
+       menuToggle.classList.toggle('active')
+       navugation.classList.toggle('active')
 
-    };
+     };
 
 
-    $(function(){
-  $('.slider').slick({
-    dots: false,
-      infinite: true,
+     $(function(){
+   $('.slider').slick({
+     dots: false,
+       infinite: true,
       speed: 1500,
       autoplay: true,
-      arrows: false    
+       arrows: false    
+  });
  });
-});
   
+const menuLinks = document.querySelectorAll('.header__link[data-goto]');
+if (menuLinks.length > 0) {
+  menuLinks.forEach(menuLinks => {
+    menuLinks.addEventListener("click", onMenuLinkClick);
+  });
+  function onMenuLinkClick(e) {
+    const  menuLink = e.target;
+    if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+const gotoBlock = document.querySelector(menuLink.dataset.goto);
+const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
+
+    window.scrollTo({
+top: gotoBlockValue,
+behavior: "smooth"
+
+
+    });
+    e.preventDefault();
+
+    }
+  }
+}
